255ca5297fece19a85a6d39642bc47e33641f4b0
root <root@DESKTOP-V10F18E.localdomain>
2024-11-12 18:02:03 -0500

    Very good class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 //import './App.css';
 import Wrapper from './components/Wrapper.js';
 import Screen from './components/Screen.js';
 import ButtonBox from './components/ButtonBox.js';
 import Button from './components/Button.js';
+import React, { useState } from "react";
 
 const btnValues = [
   ["C", "+-", "%", "/"],
@@ -12,31 +12,128 @@ const btnValues = [
   [1, 2, 3, "+"],
   [0, ".", "="],
 ];
+const toLocaleString = (num) =>
+  String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
 
-function App() {
+const removeSpaces = (num) => num.toString().replace(/\s/g, "");
+
+const App = () => {
+  let [calc,setCalc]= useState ({
+    sign:"",num:5,res:0
+  })
+  const numClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    if (removeSpaces(calc.num).length < 16) {
+      setCalc({
+        ...calc,
+        num:
+          calc.num === 0 && value === "0"
+            ? "0"
+            : removeSpaces(calc.num) % 1 === 0
+            ? toLocaleString(Number(removeSpaces(calc.num + value)))
+            : toLocaleString(calc.num + value),
+        res: !calc.sign ? 0 : calc.res,
+      });
+    }
+  };
+  const commaClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+  
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+    });
+  };
+  const signClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+  
+    setCalc({
+      ...calc,
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0,
+    });
+  };
+  const equalsClickHandler = () => {
+    if (calc.sign && calc.num) {
+      const math = (a, b, sign) =>
+        sign === "+"
+          ? a + b
+          : sign === "-"
+          ? a - b
+          : sign === "X"
+          ? a * b
+          : a / b;
+  
+      setCalc({
+        ...calc,
+        res:
+          calc.num === "0" && calc.sign === "/"
+            ? "Can't divide with 0"
+            : math(Number(calc.res), Number(calc.num), calc.sign),
+        sign: "",
+        num: 0,
+      });
+    }
+  };
+  const invertClickHandler = () => {
+    setCalc({
+      ...calc,
+      num: calc.num ? calc.num * -1 : 0,
+      res: calc.res ? calc.res * -1 : 0,
+      sign: "",
+    });
+  };
+  const percentClickHandler = () => {
+    let num = calc.num ? parseFloat(calc.num) : 0;
+    let res = calc.res ? parseFloat(calc.res) : 0;
+  
+    setCalc({
+      ...calc,
+      num: (num /= Math.pow(100, 1)),
+      res: (res /= Math.pow(100, 1)),
+      sign: "",
+    });
+  };
+  const resetClickHandler = () => {
+    setCalc({
+      ...calc,
+      sign: "",
+      num: 0,
+      res: 0,
+    });
+  };
+
   return (
 
-        <Wrapper>
-          <Screen value={"Hello"}></Screen>
-          <ButtonBox>
-            {
-              btnValues.flat().map((btn, i) => {
-              return(
-                <Button key={i} className={btn === "=" ? "equals" : ""} value = {btn} onClick ={()=>{
-                  console.log(`${btn} clicked!`)
-                }}></Button>
-              )
-              }
-            )
-            }
-          </ButtonBox>
-        </Wrapper>
+    <Wrapper>
+      <Screen value={calc.num ? calc.num : calc.res}></Screen>
+      <ButtonBox>
+        {
+          btnValues.flat().map((btn, i) => {
+          return(
+            <Button key={i} className={btn === "=" ? "equals" : ""} value = {btn} 
+            onClick ={btn ==="C" ? resetClickHandler : 
+              btn==="+-" ? invertClickHandler : console.log("pass invert") && btn ==="%"
+              ? percentClickHandler : btn ==="="
+              ? equalsClickHandler : btn==="/" || btn==="X" || btn==="-"|| btn ==="+"
+              ? signClickHandler : btn==="."
+              ? commaClickHandler : numClickHandler
+            }></Button>
+          )
+          }
+        )
+        }
+      </ButtonBox>
+    </Wrapper>
 
 
-  );
-}
-
-
+);
+  }
 
 
 export default App;
